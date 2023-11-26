2f806965b5f82abd7c4d6541e9b98cbcfb39fb3e
Salmaan Saeed <salsaeed999@gmail.com>
2023-11-26 11:01:26 -0500

    added exceptions to the script

--- lint_with_clippy.sh
+++ lint_with_clippy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # lint the project using clippy
-cargo clippy --all -- -W clippy::all -W clippy::pedantic -W clippy::nursery -W clippy::cargo -W clippy::unwrap_used -W clippy::missing_docs_in_private_items -A clippy::multiple_crate_versions -D warnings
+cargo clippy --all -- -W clippy::all -W clippy::pedantic -W clippy::nursery -W clippy::cargo -W clippy::unwrap_used -A clippy::multiple_crate_versions -A clippy::too-many-lines -A clippy::module-name-repetitions -D warnings
